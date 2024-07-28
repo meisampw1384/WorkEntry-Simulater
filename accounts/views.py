484f5fedd99eb1537d80d7e61acf2c660653a3a4
meisampw1384 <mpwryamehr585@gmail.com>
2024-07-28 14:45:01 +0330

    fix(view) : enhance the view of the work_entry_app

--- accounts/views.py
+++ accounts/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render, redirect
 from django.views.generic import View, TemplateView
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
-from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
+from django.utils.decorators import method_decorator
+
 
 from .forms import SignUpForm
 
 class SignUpView(View):
-    @method_decorator(never_cache)
+
     def get(self, request):
         form = SignUpForm()
         return render(request, 'signup.html', {'form': form})
     
-    @method_decorator(never_cache)
+
     def post(self, request):
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -23,12 +24,12 @@ class SignUpView(View):
         return render(request, 'signup.html', {'form': form})
 
 class LoginView(View):
-    @method_decorator(never_cache)
+ 
     def get(self, request):
         form = AuthenticationForm()
         return render(request, 'login.html', {'form': form})
     
-    @method_decorator(never_cache)
+
     def post(self, request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
@@ -37,14 +38,12 @@ class LoginView(View):
             return redirect('board_list')
         return render(request, 'login.html', {'form': form})
 
+
+
 class LogoutView(View):
-    @method_decorator(never_cache)
     def get(self, request):
         logout(request)
         return redirect('home')
 
 class HomeView(TemplateView):
     template_name = 'home.html'
-    @method_decorator(never_cache)
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
--- work_entry_app/views.py
+++ work_entry_app/views.py
@@ -1,48 +1,57 @@
 from typing import Any
-
-
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import View, ListView, DetailView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
 from django.utils import timezone
-
-
+from django.contrib.auth import login, logout
 from .models import Board, List, Card
 from .forms import ListForm, BoardForm, CardForm
 
+
+@method_decorator(never_cache, name='dispatch')
 class BoardListView(LoginRequiredMixin, ListView):
     model = Board
     template_name = 'board_list.html'
     context_object_name = 'boards'
-    login_url = '/login/'  
-    
-    @method_decorator(never_cache)
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
+    login_url = '/login/'
 
     def get_queryset(self):
         return Board.objects.filter(owner=self.request.user)
 
+
+@method_decorator(never_cache, name='dispatch')
 class BoardDetailView(LoginRequiredMixin, DetailView):
     model = Board
     template_name = 'board_detail.html'
     context_object_name = 'board'
-
-    @method_decorator(never_cache)
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
+    login_url = '/login/'
     
+    def get_object(self, queryset=None):
+        try:
+            return super().get_object(queryset)
+        except:
+            return None
+        
+        
+    def get(self,request,*args,**kwargs):
+        board = self.get_object()
+        if not board :
+            return redirect('board_list')
+        return super().get(request,*args,**kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         board = self.get_object()
         context['lists'] = board.lists.prefetch_related('cards')
         context['list_form'] = ListForm()
         context['card_form'] = CardForm()
-        context['timezone_now'] = timezone.now()  
+        context['timezone_now'] = timezone.now()
         return context
-    #for delete the board
+
     def post(self, request, *args, **kwargs):
         board = self.get_object()
         if 'delete_board' in request.POST:
@@ -51,13 +60,15 @@ class BoardDetailView(LoginRequiredMixin, DetailView):
         return super().post(request, *args, **kwargs)
 
 
+@method_decorator(never_cache, name='dispatch')
 class CreateBoardView(LoginRequiredMixin, View):
-    @method_decorator(never_cache)
+    
+    login_url = '/login/'
+
     def get(self, request):
         form = BoardForm()
         return render(request, 'create_board.html', {'form': form})
-    
-    @method_decorator(never_cache)
+
     def post(self, request):
         form = BoardForm(request.POST)
         if form.is_valid():
@@ -67,8 +78,11 @@ class CreateBoardView(LoginRequiredMixin, View):
             return redirect('board_detail', pk=board.pk)
         return render(request, 'create_board.html', {'form': form})
 
+
 class ListView(LoginRequiredMixin, View):
-    @method_decorator(never_cache)
+    
+    login_url = '/login/'
+
     def post(self, request, board_id):
         board = Board.objects.get(pk=board_id)
         form = ListForm(request.POST)
@@ -78,27 +92,27 @@ class ListView(LoginRequiredMixin, View):
             list_instance.save()
             return redirect('board_detail', pk=board.pk)
         return redirect('board_detail', pk=board.pk)
-    
 
 
 class CardView(LoginRequiredMixin, View):
-    @method_decorator(never_cache)
+    
+    login_url = '/login/'
+
     def get(self, request, list_id):
         form = CardForm()
         list_instance = List.objects.get(pk=list_id)
         return render(request, 'create_card.html', {'form': form, 'list_id': list_id, 'list': list_instance})
-    
-    @method_decorator(never_cache)
+
     def post(self, request, list_id):
         list_instance = List.objects.get(pk=list_id)
         form = CardForm(request.POST)
-        
+
         if 'mark_completed' in request.POST:
-            card = get_object_or_404(Card , pk = request.POST.get('card_id'))
+            card = get_object_or_404(Card, pk=request.POST.get('card_id'))
             card.completed = True
             card.save()
-            return redirect('board_detail', pk = list_instance.board.pk)
-        
+            return redirect('board_detail', pk=list_instance.board.pk)
+
         if form.is_valid():
             card = form.save(commit=False)
             card.list = list_instance
@@ -107,10 +121,13 @@ class CardView(LoginRequiredMixin, View):
             card.save()
             return redirect('board_detail', pk=list_instance.board.pk)
         return render(request, 'create_card.html', {'form': form, 'list_id': list_id})
-    
 
 
+@method_decorator(never_cache, name='dispatch')
 class DeleteCardView(LoginRequiredMixin, View):
+    
+    login_url = '/login/'
+    
     def post(self, request, *args, **kwargs):
         card = get_object_or_404(Card, pk=kwargs['pk'])
         if 'delete_card' in request.POST:
@@ -119,7 +136,12 @@ class DeleteCardView(LoginRequiredMixin, View):
             return redirect('board_detail', pk=board_pk)
         return redirect('board_detail', pk=card.list.board.pk)
 
+
+@method_decorator(never_cache, name='dispatch')
 class DeleteListView(LoginRequiredMixin, View):
+    
+    login_url = '/login/'
+    
     def post(self, request, *args, **kwargs):
         list_instance = get_object_or_404(List, pk=kwargs['pk'])
         if 'delete_list' in request.POST:
@@ -127,4 +149,3 @@ class DeleteListView(LoginRequiredMixin, View):
             list_instance.delete()
             return redirect('board_detail', pk=board_pk)
         return redirect('board_detail', pk=list_instance.board.pk)
-
